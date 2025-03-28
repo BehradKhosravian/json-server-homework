fdc9c87dd7cf33956cdd805ca74a40046a78f213
Behrad Khosravian <113304042+BehradKhosravian@users.noreply.github.com>
2025-03-28 21:48:21 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,16 @@
 // JSON Server module
 const jsonServer = require("json-server");
 const cors = require("cors"); // Import the cors middleware
-const helmet = require("helmet"); // Import the helmet middleware
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 
 // Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
 
-// Configure CORS to allow all origins
-server.use(cors({ origin: '*' })); // Explicitly allow all origins
-
-// Disable CSP using helmet
-server.use(
-  helmet({
-    contentSecurityPolicy: false, // Disable CSP
-  })
-);
-
+server.use(cors()); // Enable CORS for all requests
 server.use(middlewares);
-server.use(router);
 
+server.use(router);
 // Listen to port
 server.listen(3002, () => {
   console.log("JSON Server is running");
