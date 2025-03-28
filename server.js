60982663f6a5fc20bd5e08e4800650b04e417410
Behrad Khosravian <113304042+BehradKhosravian@users.noreply.github.com>
2025-03-28 21:38:41 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 // JSON Server module
 const jsonServer = require("json-server");
+const cors = require("cors"); // Import the cors middleware
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 
 // Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
 
+server.use(cors()); // Enable CORS for all requests
 server.use(middlewares);
 
 server.use(router);
 // Listen to port
-server.listen(3001, () => {
+server.listen(3002, () => {
  console.log("JSON Server is running");
 });
 
