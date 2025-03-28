bf6d426f948f643efe526dfc814607ea605d6f1e
Behrad Khosravian <113304042+BehradKhosravian@users.noreply.github.com>
2025-03-28 20:16:33 +0330

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
-// JSON Server module
-const jsonServer = require("json-server");
+const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-
-// Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
-
 server.use(router);
-// Listen to port
-server.listen(3001, () => {
- console.log("JSON Server is running");
-});
 
-// Export the Server API
-module.exports = server;
+server.listen(port);
