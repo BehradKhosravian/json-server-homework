4e37a32578d63077e1244fad7491e6a9ba3360ac
Behrad Khosravian <113304042+BehradKhosravian@users.noreply.github.com>
2025-03-28 20:14:32 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const jsonServer = require("json-server"); // importing json-server library
+// JSON Server module
+const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
+
+// Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
-server.use(router);
 
-server.listen(port);
+server.use(router);
+// Listen to port
+server.listen(3001, () => {
+ console.log("JSON Server is running");
+});
+
+// Export the Server API
+module.exports = server;
